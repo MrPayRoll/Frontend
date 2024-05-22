9211c0de43c53daf4f1eafd477647fc82680fb64
MrPayRoll <ggtoper823@gmail.com>
2024-05-22 23:41:44 +0300

    four

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,19 @@ import YourComponent from './Components/test';
 
 export default function App() {
   return (
-    <Router> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/basket" element={<Basket />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/footer" element={<Footer />} />
-        <Route path="/header" element={<Header />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/onas" element={<Onas />} />
-        <Route path="/modal" element={<Modal />} />
-        <Route path="/test" element={<YourComponent />} />
-        
-      </Routes>
-    </Router> 
+      <Router> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/basket" element={<Basket />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/footer" element={<Footer />} />
+          <Route path="/header" element={<Header />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/onas" element={<Onas />} />
+          <Route path="/modal" element={<Modal />} />
+          <Route path="/test" element={<YourComponent />} />
+        </Routes>
+      </Router> 
   );
 }
